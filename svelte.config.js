c12c81484eff7b22c0f3b0d946326a46cb92766c
andreas <andreas.sch4@gmail.com>
2025-12-04 10:57:10 +0100

    feat: configurable prerender origin

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const PRERENDER_ORIGIN = process.env.PRERENDER_ORIGIN || 'https://your-domain.com';
+
 export default {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
@@ -10,6 +12,9 @@ export default {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			origin: PRERENDER_ORIGIN
+		}
 	}
 }
