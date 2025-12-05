7cb23e3bf44637be6e539ce32a2a49e75082e5cc
andreas <andreas.sch4@gmail.com>
2025-12-05 16:23:11 +0100

    fix: client redirect on wrong language

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,12 @@
+import { shouldRedirect } from '$lib/paraglide/runtime';
+import { redirect } from '@sveltejs/kit';
+
 export const prerender = true;
+
+export const load = async ({ url }) => {
+	const decision = await shouldRedirect({ url });
+
+	if (decision.shouldRedirect && decision.redirectUrl) {
+		redirect(302, decision.redirectUrl.href);
+	}
+};
