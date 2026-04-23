a4bc509a3bb809dd8361f4daf0cec4e141f09150
andreas <andreas.sch4@gmail.com>
2026-04-23 20:18:14 +0200

    fix: broken lang redirect on prerendered sites

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,16 @@
+import { browser } from '$app/environment';
+import { shouldRedirect } from '$lib/paraglide/runtime';
+import { redirect } from '@sveltejs/kit';
+import { removeTrailingSlash } from '$lib/url';
+
 export const prerender = true;
+
+export const load = async ({ url }) => {
+	if (!browser) return;
+
+	const decision = await shouldRedirect({ url });
+
+	if (decision.shouldRedirect && decision.redirectUrl) {
+		redirect(302, removeTrailingSlash(decision.redirectUrl.href));
+	}
+};
